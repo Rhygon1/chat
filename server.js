7ee5947933b50e75c983f8cda814c338fca61b18
brungmail <bruh@gmail.com>
2023-07-19 19:29:28 -0700

    hi

--- server.js
+++ server.js
@@ -20,7 +20,22 @@ const vapidKeys = {
     privateKey: 'vqqrlrAwVPqHHPet5j1j2CqGVPQaBAgoNn0K9-QKIU8',
 };
 
-const convertedVapidKey = webpush.urlBase64ToUint8Array(vapidKeys.publicKey);
+// Helper function to convert URL-safe Base64 to Uint8Array
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+  
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+  
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+  
+    return outputArray;
+  }
+
+const convertedVapidKey = urlBase64ToUint8Array(vapidKeys.publicKey);
 
 webpush.setVapidDetails(
     'mailto:dhruvgoel160207@gmail.com',
